e727949d44664d5332db41db4712cfaf4bbd49fb
Daniel Weber <dw@thedanielweber.com>
2016-12-02 16:44:42 +0000

    Improved efficiency of checking phrases by added an array of phrases to check

--- src/trumpit.js
+++ src/trumpit.js
@@ -1,13 +1,15 @@
 function isTrumped(s) {
     
-    // Check if string contains the string "great again"
-    if(s.toLowerCase().indexOf("great again") > -1) {
-        return true;
-    }
+    var phrases = [
+        "great again",
+        "build a wall"
+    ];
     
-    // Check if string contains the string "build a wall"
-    if(s.toLocaleLowerCase().indexOf("build a wall") > -1) {
-        return true;
+    for( var i = 0; i < phrases.length; i++ ) {
+        console.log("Testing phrase: " + phrases[i]);
+        
+        // check string passes in again each of these phrases. If it matches one, it will return true
+        if(s.toLowerCase().indexOf(phrases[i]) > -1) return true;
     }
    
     // return false if it does not match any of our cases
